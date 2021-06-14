1de13208c438ad0f46cfe999c2c1d90a403cdf80
israel <israelbrito9430@gmail.com>
2021-06-14 08:09:28 -0400

    variables

--- sprint-one/src/components/CommentForm.jsx
+++ sprint-one/src/components/CommentForm.jsx
@@ -3,9 +3,9 @@ import UserPic from '../assets/images/Mohan-muruge.jpg'
 import './CommentForm.scss'
 const CommentForm = () => {   
   return (
-    <div>
-      <img src={UserPic} alt="User Pic" style={{ width: '60px', borderRadius: '50%', height: '50px' }} />
-      <div>
+    <div className="CommentForm">
+      <img className="CommentForm__pic" src={UserPic} alt="User Pic" style={{ width: '60px', borderRadius: '50%', height: '50px' }} />
+      <div className="CommentForm__box" >
         <div>JOIN THE CONVERSATION</div>
         <input type="text" placeholder="Write comment here" />
       </div>
